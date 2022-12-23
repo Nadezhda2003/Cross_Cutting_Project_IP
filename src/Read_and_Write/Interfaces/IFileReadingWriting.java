596cf746d5822c83aa111a4d9ea391427e8e1a17
Nadezhda Bazhanova <nadia.bazhanova@gmail.com>
2022-12-23 22:09:52 +0300

    Change Xml reader and writer, add tests for files

--- src/Read_and_Write/Interfaces/IFileReadingWriting.java
+++ src/Read_and_Write/Interfaces/IFileReadingWriting.java
@@ -3,11 +3,12 @@ package Read_and_Write.Interfaces;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public interface IFileReadingWriting {
     String reading(String FileName, boolean a) throws GeneralSecurityException, IOException, ParserConfigurationException, SAXException;
-    void writing(String text, String way) throws IOException;
+    void writing(String text, String way) throws IOException, ParserConfigurationException, TransformerException;
 }
