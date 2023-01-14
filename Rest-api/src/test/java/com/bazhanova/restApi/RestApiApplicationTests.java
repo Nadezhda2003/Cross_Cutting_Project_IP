d85c8780eb8569fa5a4c7d4d606f4ca30926241e
Nadezhda Bazhanova <nadia.bazhanova@gmail.com>
2023-01-14 16:54:44 +0300

    master

--- Rest-api/src/test/java/com/bazhanova/restApi/RestApiApplicationTests.java
+++ Rest-api/src/test/java/com/bazhanova/restApi/RestApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bazhanova;
+package com.bazhanova.restApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BazhanovaApplicationTests {
+class RestApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
